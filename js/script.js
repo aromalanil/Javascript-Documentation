bdd126fb3c9075b72b4f340ec24dc6e3ffa4ef4e
Aromal Anil <aromalanilkannan@gmail.com>
2020-01-28 07:38:27 +0530

    prototype finished

--- js/script.js
+++ js/script.js
@@ -1,10 +1,41 @@
+/**
+ * @author Aromal Anil <github.com/aromalanil>
+ */
+
+
 $(document).ready(()=>{
 
 
-    $('#menu').click(()=>
-    {
-        console.log('click')
+    //NavBar Drop Shadow on scroll
+    $(window).scroll(function() {
+        if ($(window).scrollTop() > 10) {
+            $('.mobile-logo').addClass('floatingNav');
+        } else {
+            $('.mobile-logo').removeClass('floatingNav');
+        }
+    });
+
+
+
+    // Menu bar for Mobile
+    $('#menu').click(() => {
+
+        //Making Side Menu Bar Visible/Invisible On Menu Click
         $('.nav-bar').toggleClass('navBarVisible');
         
+
+
+        //Changing Menu Icon
+        let img = $('#menu').attr('src');
+        $('#menu').attr('src', img === './img/menu.svg' ? './img/close.svg' : './img/menu.svg');
+
+
+        //Making Side Menu Bar Visible/Invisible When Clicked On Link
+        if ($('.nav-bar').hasClass('navBarVisible')) {
+            $('.nav-link').click(() => {
+                $('.nav-bar').removeClass('navBarVisible');
+                $('#menu').attr('src', './img/menu.svg');
+            })
+        }
     })
 })
